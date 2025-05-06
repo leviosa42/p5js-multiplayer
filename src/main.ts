e94e553dd2310cb9925cd41c6f79a609ef2cf472
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-05-06 15:11:19 +0900

    update: initでいらないファイルやコードを削除しp5js用に改造

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <img src="/vite-deno.svg" alt="Vite with Deno" />
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import p5 from 'p5'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const sketch = (p5: p5) => {
+
+  p5.windowResized = () => {
+    const parent = p5.canvas.parentElement!;
+    p5.resizeCanvas(parent.clientWidth, parent.clientHeight);
+  };
+
+  p5.setup = () => {
+    const parent = p5.canvas.parentElement!;
+    p5.createCanvas(parent.clientWidth, parent.clientHeight);
+  };
+
+  p5.draw = () => {
+    p5.background(220);
+    // p5.textSize(16);
+    p5.text(`width: ${p5.width}, height: ${p5.height}`, 10, 20);
+    p5.text(`mouseX: ${p5.mouseX}, mouseY: ${p5.mouseY}`, 10, 40);
+    p5.text(`frameCount: ${p5.frameCount}`, 10, 60);
+    p5.text(`FPS: ${p5.frameRate().toFixed(2)}`, 10, 80);
+    p5.circle(p5.mouseX, p5.mouseY, 50);
+
+  };
+}
+
+new p5(sketch, document.getElementById('canvas-a') as HTMLElement);
+new p5(sketch, document.getElementById('canvas-b') as HTMLElement);
