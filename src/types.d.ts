19ef699f681eae97843fdf7766f56705bb9c11d8
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-05-06 14:39:24 +0900

    fix: p5.canvasの型エラーを防ぐためにパッチを当てる

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+import _p5 from "p5";
+declare module "p5" {
+  // NOTE: psInstanceExtension という拡張用と思われる便利なinterfaceがあるので、これを使う
+  interface p5InstanceExtensions {
+    // WHY:
+    //   p5.canvas は p5.js の内部で定義されているが、@types/p5 では定義されていない．
+    //   これを定義することで、p5.canvas を使用できるようにする．
+    canvas: HTMLCanvasElement;
+  }
+}
