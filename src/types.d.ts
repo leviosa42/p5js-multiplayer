10221146f7f9da3f425c69b6693d6e71033cc1c4
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-05-09 01:53:10 +0900

    wip: p5.touchesの型定義をobject[]から変える

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,12 @@
 import _p5 from "p5";
+
+interface Touch {
+  x: number;
+  y: number;
+  winX: number;
+  winY: number;
+  id: number;
+}
 declare module "p5" {
   // NOTE: psInstanceExtension という拡張用と思われる便利なinterfaceがあるので、これを使う
   interface p5InstanceExtensions {
@@ -6,5 +14,7 @@ declare module "p5" {
     //   p5.canvas は p5.js の内部で定義されているが、@types/p5 では定義されていない．
     //   これを定義することで、p5.canvas を使用できるようにする．
     canvas: HTMLCanvasElement;
+    // REF: https://github.com/processing/p5.js/blob/31aebe1fc38153dffa34627aa5490e328dbe06f1/src/events/touch.js#L111C1-L123C1
+    // touches: Touch[];
   }
 }
