07528b78581318151c46f7215f4e5e0fbc13a263
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-05-23 09:44:22 +0900

    update

--- src/sketches/controller.ts
+++ src/sketches/controller.ts
@@ -1,10 +1,50 @@
 import p5 from "p5";
+import mqtt from "mqtt";
 
 import { Joystick } from "./components/Joystick.ts";
 import { Touch } from "../types.d.ts";
 import { Player } from "./components/Player.ts";
+// import { Buffer } from "node:buffer";
 
+interface PlayerDataModel {
+  position: {
+    x: number;
+    y: number;
+  };
+  velocity: {
+    x: number;
+    y: number;
+  };
+  clientId: string;
+  timestamp: number;
+}
 export const sketch = (p5: p5) => {
+  const MQTT_URL = "http://localhost:1884";
+  const MQTT_TOPIC = "game";
+  const MQTT_OPTIONS = {
+    clientId: "mqttjs_" + Math.random().toString(16).substr(2, 8),
+  };
+  let mqttClient: mqtt.MqttClient | null = null;
+  mqttClient = mqtt.connect(MQTT_URL, MQTT_OPTIONS);
+  mqttClient.on("connect", () => {
+    console.log("Connected to MQTT broker");
+    mqttClient!.subscribe(MQTT_TOPIC, (err: Error | null) => {
+      if (err) {
+        console.error("Failed to subscribe to topic:", err);
+      } else {
+        console.log("Subscribed to topic:", MQTT_TOPIC);
+      }
+    });
+  });
+  mqttClient.on("message", (_topic: string, payload: any) => {
+    const data: PlayerDataModel = JSON.parse(
+      payload.toString(),
+    ) as PlayerDataModel;
+    console.log("Received data:", data);
+    onlinePlayers.set(data.clientId, data);
+  });
+  const onlinePlayers: Map<string, PlayerDataModel> = new Map();
+
   let joystick: Joystick | null = null;
   // let playerV: p5.Vector | null = null; // mockup player variable
   let player: Player | null = null;
@@ -43,6 +83,7 @@ export const sketch = (p5: p5) => {
       radius: p5.height * 0.1,
       effectiveRadius: p5.height * 0.15,
     });
+
     // playerV = p5.createVector(p5.width / 2, p5.height / 2);
     player = new Player({
       p5,
@@ -83,5 +124,38 @@ export const sketch = (p5: p5) => {
 
     player!.update();
     player!.draw();
+    // other players
+    onlinePlayers.forEach((data: PlayerDataModel, clientId: string) => {
+      p5.push();
+      p5.fill("#008800");
+      p5.ellipse(data.position.x, data.position.y, 20);
+      p5.text(
+        `lag: ${Date.now() - data.timestamp}`,
+        data.position.x,
+        data.position.y,
+      );
+      p5.pop();
+    });
+
+    mqttClient!.publish(
+      MQTT_TOPIC,
+      JSON.stringify({
+        position: {
+          x: player!.positionV.x,
+          y: player!.positionV.y,
+        },
+        velocity: {
+          x: player!.velocityV.x,
+          y: player!.velocityV.y,
+        },
+        clientId: mqttClient!.options.clientId,
+        timestamp: Date.now(),
+      }),
+    );
+
+    // debug
+    p5.text(`mqtt clientId: ${mqttClient!.options.clientId}`, 10, 80);
+    // debug
+    p5.text(`frameRate: ${p5.frameRate().toFixed(2)}`, 10, 20);
   };
 };
